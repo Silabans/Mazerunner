38cf8268de838b23d35d09523c93c31aa883d5e0
Matthew Zimra Yedija Silaban <matthew.yedija@gmail.com>
2026-05-16 19:47:59 +0800

    add a path reconstruction feature into main.py to visualise the shortest path taken by the maze solver. While doing so, i faced a bug where the traceback was inaccurate so it displayed the unclean path instead of the shortest one. Fixed by adding the traceback when pushing instead of popping

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ options = Queue()
 
 # visited coordinates
 visited = set()
-
+seen = set()
 step = 1
+
+# This dictionary records the key as the contemporary next coordinates
+# and the value as the coordinates from which we come to reach the them
+traceback = {}
 
 while here != maze.end_coor:
     print(f"========== STEP {step} ==========")
@@ -31,8 +35,10 @@ while here != maze.end_coor:
     around = maze.around(here)
 
     for coor in around:
-        if coor not in visited:
+        if coor not in visited and coor not in seen:
             options.push(coor)
+            seen.add(coor)
+            traceback[coor] = here
 
     try:
         here = options.pop()
@@ -40,4 +46,43 @@ while here != maze.end_coor:
     except IsEmptyError:
         raise IsEmptyError('No solution; no more paths to choose from')
     
+    sleep(0.2)
+visited.add(here)
+
+# shortest path taken reconstructed
+path = []
+current = maze.end_coor
+
+while current != maze.start_coor:
+    path.append(current)
+    current = traceback[current]
+    print(current)
+path.append(maze.start_coor)
+path.reverse()
+
+print(f"PATH: {path}")
+print(f'path length: {len(path)}')
+print(f'number of visited: {len(visited)}')
+print(f'length of seen: {len(seen)}')
+
+
+reconstructed = Queue()
+total_steps_required = len(path)
+
+
+loading_time = 1.5
+dot_count = 3
+while loading_time > 0:
+    print(f"Loading final path taken {'.' * dot_count}   ", end='\r', flush=True)
     sleep(0.5)
+    dot_count -= 1
+    loading_time -= 0.5
+
+
+print(f"========== STEPS REQUIRED TO FINISH: {total_steps_required} ==========")
+maze.display(
+    here=maze.start_coor,
+    options=None,
+    visited=set(path)
+)
+
